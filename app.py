27094a8835cb5b13e4244c8f98c2a9615f129a9b
philippe jamelot <philippe.jamelot@gmail.com>
2020-04-16 17:17:40 +0200

    json 1.8

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
      return "Hello World!"
+
+@app.route('/add_two_nums', methods=["POST"])
+def add_two_nums():
+    # Get x,y from the posted data
+    dataDict = request.get_json()
+#    return jsonify(dataDict)
+ 
+    if "y" not in dataDict:
+        return "Error", 305
+
+    x = dataDict["x"]
+    y = dataDict["y"]
+
+
+    #Add Z=x+y
+    z = x+y
+
+    #Prepare JSON
+    retJSON = {
+        "z":z
+    }
+
+    #return jsonify(map_prepared)
+    return jsonify(retJSON), 200
+
+
 
 @app.route('/bye')
 def bye():
